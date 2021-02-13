be4f0c8036f3292d81308dc12adeb6f5f8e5ab85
Edwinmoropane <Edwinmoropane@gmail.com>
2021-02-13 19:19:22 +0300

    First comit

---
+++ HotelListing/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { 
+            //Logger configuration
+            Log.Logger = (Serilog.ILogger)new LoggerConfiguration()
+                .WriteTo.File(
+                path: "c:\\hotellistings\\logs\\log-.txt",
+                outputTemplate: "{Timestamp:yyyy-mm-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:1j}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day,
+                restrictedToMinimumLevel: LogEventLevel.Information
+                ).CreateLogger(); //setting up default behaviors for our Seri logger
+
+            try
+            {
+                Log.Information("Application is starting");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+
+                Log.Fatal(ex, "Application Failed to start");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+            
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog() //user Seri logger
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
